7cae760b45fb7049d29c06c2a7f4167f1715b2af
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-01 12:44:35 +0200

    RegExp laboratorio finish

--- 07-RegExp/laboratorio/app.js
+++ 07-RegExp/laboratorio/app.js
@@ -56,3 +56,95 @@ for(let plate of platesArray){
     console.log(`Plate: ${fullPlate}, number: ${numberPlate}, letters: ${lettersPlate}`)
     
 }
+
+//Optional Excercises
+
+//Optional1
+
+// Case 1
+
+const imgTagString = '<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>'
+
+const Op1Case1 = /src=['"](.*)["']/
+
+console.log(Op1Case1.exec(imgTagString)[1])
+
+// Case 2
+
+const htmlString = `
+<html>
+<body>
+<img
+src="https://image.freepik.com/iconos-gratis/github-circular_318-10610.jpg"
+/>
+<h1>ejemplo</h1>
+<img
+src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+/>
+</body>
+</html>`
+
+const Op1Case2 = /<img[^>]+src="([^">]+)"/
+const Op1Case2g = /<img[^>]+src="([^">]+)"/g
+
+const imgTagsArray = htmlString.match(Op1Case2g);
+
+for(let imgTag of imgTagsArray){
+    console.log(Op1Case2.exec(imgTag)[1])
+}
+
+/**
+ * Note: Not happy with result. Parse html is complicated and there are lot of edge cases.
+ * For example the use of "" or ''
+ * Luckily browsers do parse html to create the DOM. Below you can find how I would do it in a real case scenario with JS.
+ */
+
+const imgTags = document.getElementsByTagName("img");
+
+for(let imgTag of imgTags){
+    console.log(imgTag.src)
+}
+
+//Optional 2
+
+//Case 1
+const creditCardArray = ["5299640000000000", "5299-6400-0000-0000", "5299 6400 0000 0000" ]
+
+const op2Case1 = /^5[0-5]\d{2}[\s-]?(\d{4}[\s-]?){2}\d{4}$/
+
+for(let creditCard of creditCardArray){
+    console.log(`Credit Card ${creditCard} is valid:`,op2Case1.test(creditCard))
+}
+
+//Case 2
+
+const creditCard = "5299640000000000"
+
+const op2Case2 = /^(5[0-5]\d{2})[\s-]?(\d{4})[\s-]?(\d{4})[\s-]?(\d{4})/
+
+const CreditCardDivided = op2Case2.exec(creditCard)
+
+console.log(CreditCardDivided)
+
+
+
+//*************** */
+
+//Community RegExp
+
+//Hex color 3-6 digits # optional
+
+const hexPatter = /^#?([0-9a-f]{3}){1,2}$/
+
+// url validation
+
+const urlPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+
+// moderate password
+
+const moderatePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
+//strong Password
+const strongPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$/
+
+
