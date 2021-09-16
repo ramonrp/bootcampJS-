b35a16b3fd214b13771546ac48cf5817a61258ac
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-16 08:39:54 +0200

    refactor getMovementsPerAccountID

--- 12-banca-online/src/pages/movements/movements.api.js
+++ 12-banca-online/src/pages/movements/movements.api.js
@@ -3,9 +3,7 @@ import Axios from 'axios';
 const url = `${process.env.BASE_API_URL}/movements`;
 
 export const getMovementsPerAccountID = (accountId) => {
-  return Axios.get(url)
-    .then((response) => response.data)
-    .then((data) => {
-      return data.filter((movement) => movement.accountId === accountId);
-    });
+  return Axios.get(url, { params: { accountId } }).then(
+    (result) => result.data
+  );
 };
