ea819ec0c30a9e7810a00876082fc5c1f86ef71c
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-16 14:50:57 +0200

    add constants for options of select synchronus

--- 13-portal-inmobiliario/src/pages/property-list/property-list.js
+++ 13-portal-inmobiliario/src/pages/property-list/property-list.js
@@ -5,11 +5,21 @@ import {
 } from './property-list.api';
 import { addPropertyRows, setOptions } from './property-list.helpers';
 import { propertyListFromApiToVm } from './property-list.mappers';
+import {
+  roomOptions,
+  bathOptions,
+  maxPrice,
+  minPrice,
+} from './property-list.constants';
 
 Promise.all([getPropertyList(), getSaleType(), getProvinces()]).then(
   ([propertyListApi, saleTypeList, provincesList]) => {
     addPropertyRows(propertyListFromApiToVm(propertyListApi));
     setOptions(saleTypeList, 'select-sale-type', '¿Qué venta?');
     setOptions(provincesList, 'select-province', '¿Dónde?');
+    setOptions(roomOptions, 'select-room', '¿Habitaciones?');
+    setOptions(bathOptions, 'select-bathroom', '¿Baños?');
+    setOptions(minPrice, 'select-min-price', 'Min (EUR)');
+    setOptions(maxPrice, 'select-max-price', 'Max (EUR)');
   }
 );
