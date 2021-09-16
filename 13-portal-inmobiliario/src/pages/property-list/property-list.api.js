30f34057bdce8aa17a793bdd813db44c7dd5ad25
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-16 13:27:31 +0200

    add getSaleType / getProvinces function

--- 13-portal-inmobiliario/src/pages/property-list/property-list.api.js
+++ 13-portal-inmobiliario/src/pages/property-list/property-list.api.js
@@ -1,9 +1,17 @@
 import Axios from 'axios';
 
-const url = `${process.env.BASE_API_URL}/properties`;
+const url = `${process.env.BASE_API_URL}`;
 
 export const getPropertyList = () => {
-  return Axios.get(url).then((response) => {
+  return Axios.get(`${url}/properties`).then((response) => {
     return response.data;
   });
+};
+
+export const getSaleType = () => {
+  return Axios.get(`${url}/saleTypes`).then((response) => response.data);
+};
+
+export const getProvinces = () => {
+  return Axios.get(`${url}/provinces`).then((response) => response.data);
 };
--- 13-portal-inmobiliario/src/pages/property-list/property-list.js
+++ 13-portal-inmobiliario/src/pages/property-list/property-list.js
@@ -1,7 +1,19 @@
-import { getPropertyList } from './property-list.api';
-import { addPropertyRows } from './property-list.helpers';
+import {
+  getPropertyList,
+  getSaleType,
+  getProvinces,
+} from './property-list.api';
+import { addPropertyRows, setOptions } from './property-list.helpers';
 import { propertyListFromApiToVm } from './property-list.mappers';
 
 getPropertyList().then((propertyListApi) => {
   addPropertyRows(propertyListFromApiToVm(propertyListApi));
+});
+
+getSaleType().then((saleTypeList) => {
+  setOptions(saleTypeList, 'select-sale-type', '¿Qué venta?');
+});
+
+getProvinces().then((provincesList) => {
+  setOptions(provincesList, 'select-province', '¿Dónde?');
 });
