1b9be3f076f168574272275a3e908d7c97162244
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 09:32:27 +0200

    add onUpdateFields toForm

--- 13-portal-inmobiliario/src/pages/property-detail/property-detail.js
+++ 13-portal-inmobiliario/src/pages/property-detail/property-detail.js
@@ -1,11 +1,40 @@
 import { history } from '../../core/router';
 import { getPropertyPerId } from './property-detail.api';
 import { setPropertyValues } from './property-detail.helpers';
-
+import { propertyDetailFromApiToVm } from './property-detail.mappers';
+import { onUpdateField, onSubmitForm } from '../../common/helpers';
 const params = history.getParams();
+
 if (params.id) {
   getPropertyPerId(params.id).then((propertyArray) => {
     if (propertyArray[0]) {
+      const propertyVm = propertyDetailFromApiToVm(propertyArray[0]);
+      setPropertyValues(propertyVm);
     }
   });
 }
+
+let contact = {
+  email: '',
+  message: '',
+};
+
+onUpdateField('email', (e) => {
+  const value = e.target.value;
+  contact = {
+    ...contact,
+    email: value,
+  };
+});
+
+onUpdateField('message', (e) => {
+  const value = e.target.value;
+  contact = {
+    ...contact,
+    message: value,
+  };
+});
+
+onSubmitForm('contact-button', () => {
+  console.log(contact);
+});
