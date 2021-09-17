b31760d27a8ae5a34db2c24438d3e60da5aca465
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 10:59:15 +0200

    add upload-property.api.js

---
+++ 13-portal-inmobiliario/src/pages/upload-property/upload-property.api.js
@@ -0,0 +1,11 @@
+import Axios from 'axios';
+
+const url = `${process.env.BASE_API_URL}`;
+
+export const getSaleType = () => {
+  return Axios.get(`${url}/saleTypes`).then((response) => response.data);
+};
+
+export const getProvinces = () => {
+  return Axios.get(`${url}/provinces`).then((response) => response.data);
+};
