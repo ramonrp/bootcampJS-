1fbf71b31bb0f1385b8f0c87edd6e3444874d058
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 10:26:07 +0200

    add updateField Listeners to general data form

--- 13-portal-inmobiliario/src/pages/upload-property/upload-property.js
+++ 13-portal-inmobiliario/src/pages/upload-property/upload-property.js
@@ -1,1 +1,53 @@
+import { onUpdateField, onSubmitForm } from '../../common/helpers';
 
+let generalData = {
+  title: '',
+  notes: '',
+  email: '',
+  phone: '',
+  price: '',
+};
+
+onUpdateField('title', (e) => {
+  const value = e.target.value;
+  generalData = {
+    ...generalData,
+    title: value,
+  };
+});
+
+onUpdateField('notes', (e) => {
+  const value = e.target.value;
+  generalData = {
+    ...generalData,
+    notes: value,
+  };
+});
+
+onUpdateField('email', (e) => {
+  const value = e.target.value;
+  generalData = {
+    ...generalData,
+    email: value,
+  };
+});
+
+onUpdateField('phone', (e) => {
+  const value = e.target.value;
+  generalData = {
+    ...generalData,
+    phone: value,
+  };
+});
+
+onUpdateField('price', (e) => {
+  const value = e.target.value;
+  generalData = {
+    ...generalData,
+    price: value,
+  };
+});
+
+onSubmitForm('save-button', () => {
+  console.log(generalData);
+});
