e58e48acc2d5723ba988579cf97b9bf95b7a609c
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 09:22:16 +0200

    create propertyDetailFromApiToVm

---
+++ 13-portal-inmobiliario/src/pages/property-detail/property-detail.mappers.js
@@ -0,0 +1,31 @@
+import { getEquipments } from './property-detail.api';
+
+export const propertyDetailFromApiToVm = (propertyApi) => {
+  const priceFormated = new Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  }).format(propertyApi.price);
+
+  return {
+    title: propertyApi.title,
+    notes: propertyApi.notes,
+    image: propertyApi.images[0],
+    images: propertyApi.images,
+    id: propertyApi.id,
+    price: priceFormated,
+    squareMeter: `${propertyApi.squareMeter}m²`,
+    rooms:
+      propertyApi.rooms > 1
+        ? `${propertyApi.rooms} habitaciones`
+        : `${propertyApi.rooms} habitación`,
+    bathrooms:
+      propertyApi.bathrooms > 1
+        ? `${propertyApi.bathrooms} cuartos de baño`
+        : `${propertyApi.bathrooms} cuarto de baño`,
+    mainFeatures: propertyApi.mainFeatures,
+    locationUrl: propertyApi.locationUrl,
+    equipmentIds: propertyApi.equipmentIds,
+  };
+};
