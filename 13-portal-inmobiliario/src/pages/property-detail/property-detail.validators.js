75d8c30dfecda5af4117b19873e8b2473d455a9e
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 09:45:18 +0200

    add validators for contact form

---
+++ 13-portal-inmobiliario/src/pages/property-detail/property-detail.validators.js
@@ -0,0 +1,25 @@
+import { createFormValidation, Validators } from '@lemoncode/fonk';
+
+const validatorSchema = {
+  field: {
+    email: [
+      {
+        validator: Validators.required,
+        message: 'campo requerido',
+      },
+      {
+        validator: Validators.email,
+        message: 'formato de email no válido',
+      },
+    ],
+    message: [
+      {
+        validator: Validators.maxLength,
+        customArgs: { length: 100 },
+        message: 'El mensaje no puede contener más de 100 caráteres',
+      },
+    ],
+  },
+};
+
+export const formValidator = createFormValidation(validatorSchema);
