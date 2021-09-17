3c5b75f097ba1db8130ae4606ccb0b6e5daf3429
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 09:57:00 +0200

    add insertContact function

--- 13-portal-inmobiliario/src/pages/property-detail/property-detail.api.js
+++ 13-portal-inmobiliario/src/pages/property-detail/property-detail.api.js
@@ -10,3 +10,8 @@ export const getPropertyPerId = (propertyId) =>
 const urlEquipments = `${process.env.BASE_API_URL}/equipments`;
 export const getEquipments = () =>
   Axios.get(urlEquipments).then((response) => response.data);
+
+const urlContact = `${process.env.BASE_API_URL}/contact`;
+export const insertContact = (contact) => {
+  return Axios.post(urlContact, contact).then((response) => response.data);
+};
