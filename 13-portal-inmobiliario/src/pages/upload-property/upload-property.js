30d6b7cf983421cd1121be6c562907239863a0df
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-09-17 11:58:11 +0200

    correct equipments and salesType checkBox

--- 13-portal-inmobiliario/src/pages/upload-property/upload-property.js
+++ 13-portal-inmobiliario/src/pages/upload-property/upload-property.js
@@ -1,11 +1,16 @@
 import { onUpdateField, onSubmitForm } from '../../common/helpers';
-import { getProvinces, getSaleType } from './upload-property.api';
+import {
+  getProvinces,
+  getSaleType,
+  getEquipments,
+} from './upload-property.api';
 import { setCheckboxList, setOptionList } from './upload-property.helpers';
 
-Promise.all([getProvinces(), getSaleType()]).then((result) => {
-  const [provinces, saleTypes] = result;
+Promise.all([getProvinces(), getSaleType(), getEquipments()]).then((result) => {
+  const [provinces, saleTypes, equipments] = result;
   setOptionList(provinces, 'province');
-  setCheckboxList(saleTypes, 'equipments');
+  setCheckboxList(saleTypes, 'saleTypes');
+  setCheckboxList(equipments, 'equipments');
 });
 
 let generalData = {
@@ -56,6 +61,83 @@ onUpdateField('price', (e) => {
   };
 });
 
+let propertyData = {
+  address: '',
+  city: '',
+  province: '',
+  squareMeter: '',
+  rooms: '',
+  bathrooms: '',
+  locationUrl: '',
+  newFeature: '',
+  mainFeatures: [],
+  equipments: [],
+};
+
+onUpdateField('address', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    address: value,
+  };
+});
+
+onUpdateField('province', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    provinceId: value,
+  };
+});
+
+onUpdateField('city', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    city: value,
+  };
+});
+
+onUpdateField('squareMeter', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    squareMeter: value,
+  };
+});
+
+onUpdateField('rooms', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    rooms: value,
+  };
+});
+
+onUpdateField('bathrooms', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    bathrooms: value,
+  };
+});
+
+onUpdateField('locationUrl', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    locationUrl: value,
+  };
+});
+
+onUpdateField('equipments', (e) => {
+  const value = e.target.value;
+  propertyData = {
+    ...propertyData,
+    equipments: [value],
+  };
+});
+
 onSubmitForm('save-button', () => {
-  console.log(generalData);
+  console.log(propertyData);
 });
